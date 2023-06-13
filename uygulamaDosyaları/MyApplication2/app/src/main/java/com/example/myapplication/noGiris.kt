7f7c72bb63e4d9ab521dd4c1160b228c1417145a
eyigithankul <126720305+eyigithankul@users.noreply.github.com>
2023-06-13 03:01:03 +0300

    13.06.2023(tamamlanmamış)

---
+++ uygulamaDosyaları/MyApplication2/app/src/main/java/com/example/myapplication/noGiris.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class noGiris : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_giris)
+    }
+}
